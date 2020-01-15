c9771032411c78b69414dc59f3d8c17c48537cbf
Till Junge <till.junge@altermail.ch>
2020-01-15 08:46:52 +0100

    progress

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,9 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($value: expr) => {"Hello world"};
+}
 
 fn main() {
     if my_macro!("world!") != "Hello world!" {
